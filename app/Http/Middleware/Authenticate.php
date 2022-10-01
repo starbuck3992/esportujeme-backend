787f425da2ce502ceb3bf8641249c7bd0d78602b
starbuck3992 <mcvespr@gmail.com>
2022-10-01 17:09:02 +0200

    Login, Register, Fortify fixes, Readme

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,25 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string[] ...$guards
+     * @return mixed
+     *
      */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (Auth::check()) {
+            return $next($request);
         }
+        return redirect()->away(env('SPA_URL'));
     }
 }
